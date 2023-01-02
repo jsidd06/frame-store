b6e78bbf39372fe8606418f95aa1b761ff431ea0
siddharth Jain <siddharth.jain@aaratechnoloes.ingi>
2023-01-02 14:46:27 +0530

    single product screen made it with api and implementions

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import "./index.css";
 import "./App.css";
+import { HelmetProvider } from "react-helmet-async";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
