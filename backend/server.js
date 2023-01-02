7a88fb1f478cf040bbfac0889fa6b8bb326ef835
siddharth Jain <siddharth.jain@aaratechnoloes.ingi>
2023-01-02 14:47:37 +0530

    api single product screen or read me push data

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,19 @@ import express from "express";
 import data from "./data.js";
 
 const app = express();
-// test
+// ALL PRODUCTS API
 app.get("/api/products", (req, res) => {
   res.send(data.products);
+});
+
+// SINGLE ID PRODUCT API'S
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
 });
 
 const port = process.env.PORT || 5000;
