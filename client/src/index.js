5345b63b4c2b2fae473b04d69e8a4e29dbb38b95
siddharth Jain <siddharth.jain@aaratechnoloes.ingi>
2023-01-03 13:42:42 +0530

    store made for add to cart and make it product screen and set in app.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,14 @@ import App from "./App";
 import "./index.css";
 import "./App.css";
 import { HelmetProvider } from "react-helmet-async";
+import { StoreProvider } from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <StoreProvider>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </StoreProvider>
   </React.StrictMode>
 );
