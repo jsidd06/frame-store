00e41a6d6cd817cdc50b6d6a1ea233eee43d1686
siddharth Jain <siddharth.jain@aaratechnoloes.ingi>
2023-01-03 11:02:56 +0530

    add loading box or error message box added

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import { useEffect, useReducer } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import logger from "use-reducer-logger";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Product from "../components/Product";
 import { Helmet } from "react-helmet-async";
+import MessageBox from "../components/MessageBox";
+import LoadingBox from "../components/LoadingBox";
+import { getError } from "../utils";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -33,7 +35,7 @@ function HomeScreen() {
         const result = await axios.get("/api/products");
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
-        dispatch({ type: "FETCH_ERROR", payload: err.message });
+        dispatch({ type: "FETCH_ERROR", payload: getError(err) });
       }
     };
     fetchData();
@@ -46,9 +48,9 @@ function HomeScreen() {
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
-          <div>Loading...</div>
+          <LoadingBox />
         ) : error ? (
-          <div>{error}</div>
+          <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <Row>
             {products.map((product) => (
