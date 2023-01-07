3521234ceed88a88051f703ed92904cde193158c
siddharth Jain <siddharth.jain@aaratechnoloes.ingi>
2023-01-07 10:13:03 +0530

    implement place order action with api

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose, { mongo } from "mongoose";
 import seedRouter from "./Routes/seedRoutes.js";
 import productRouter from "./Routes/productRoutes.js";
 import userRouter from "./Routes/userRoutes.js";
+import orderRouter from "./Routes/orderRoutes.js";
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
