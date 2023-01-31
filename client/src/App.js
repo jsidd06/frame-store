98e230901d9cb54c8d6714acf44a805b6ff952fa
siddharth <jsidd06@gmail.com>
2023-01-31 08:53:30 +0530

    user edit screen with api

--- client/src/App.js
+++ client/src/App.js
@@ -32,6 +32,7 @@ import ProductListScreen from "./screens/ProductListScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
 import OrderListScreen from "./screens/OrderListScreen";
 import UserListScreen from "./screens/UserListScreen";
+import UserEditScreen from "./screens/UserEditScreen";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -228,6 +229,14 @@ function App() {
                 element={
                   <AdminProtectedRoute>
                     <OrderListScreen />
+                  </AdminProtectedRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/user/:id"
+                element={
+                  <AdminProtectedRoute>
+                    <UserEditScreen />
                   </AdminProtectedRoute>
                 }
               ></Route>
